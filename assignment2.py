0814f6eb362e7e0f96a5b143d7934b5bbf105b7a
Joseph <tjogunnupe@gmail.coom>
2023-08-14 00:33:02 -0400

    Fifth Commit

--- assignment2.py
+++ assignment2.py
@@ -8,8 +8,14 @@ import numpy as np
 #Read the data in
 df = pd.read_csv('ITEC3040A2_DS.csv')
 
+
+print('///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////')
+print('///////////////////////////////////////////////////  Patients Without Heart Disease  //////////////////////////////////////////////')
+print('///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////')
+
+
 #Print the full table of attributes but only for the people with heart disease
-print('Symptoms among people who have heart disease')
+
 Heart_Disease_Symptoms = df[df['Absence/Presence'] == 1]
 print(Heart_Disease_Symptoms)
 
@@ -115,10 +121,113 @@ Not_Angina = Heart_Disease_Symptoms[Heart_Disease_Symptoms['Angia'] == 0]
 print(Not_Angina)
 
 print('///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////')
-print('///////////////////////////////////////////////////  Patients Without Heart Disease  //////////////////////////////////////////////')
+print('///////////////////////////////////////////////////  Patients With Heart Disease  /////////////////////////////////////////////////')
 print('///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////')
 
 #Printing the full table of attributes but only for those who dont have heart disease 
 No_Heart_Disease_Symptoms = df[df['Absence/Presence'] == 2]
 print(No_Heart_Disease_Symptoms)
 
+#Average age of the people who don't have heart disease
+Age1 = No_Heart_Disease_Symptoms.iloc[:,0]
+Average1 = Age1.mean()
+Min1 = Age1.min()
+Max1 = Age1.max()
+print('This is the average of the people with heart attacks in this dataset')
+print(Average1)
+
+#Max age of the people have don't heart disease 
+print('This is the max age of the people with heart attacks in this dataset')
+print(Max1)
+
+#Min age of the people that don't have heart disease 
+print('This is the min age of the people with heart attacks in this dataset')
+print(Min1)
+
+
+#Count of men 
+Men1=  No_Heart_Disease_Symptoms[No_Heart_Disease_Symptoms['Sex'] == 1]
+print('These are all of the men')
+print(Men1)
+#Count of Women 
+Women1=  No_Heart_Disease_Symptoms[No_Heart_Disease_Symptoms['Sex'] == 0]
+print('These are all of the Women')
+print(Women1)
+
+Age_Women1 = Women1.iloc[:,0]
+Average_Women1=Age_Women1.mean()
+Max_Women1 =Age_Women1.max()
+Min_Women1 = Age_Women1.min()
+print('WOMEN AGES (Avg, Max, Min):  ', Average_Women1, Max_Women1, Min_Women1)
+#Average, Max, Min age of Men to don'tget heart disease 
+Age_Men1 = Men1.iloc[:,0]
+Average_Men1=Age_Men1.mean()
+Max_Men1 =Age_Men1.max()
+Min_Men1 = Age_Men1.min()
+print('MEN AGES (Avg, Max, Min): ', Average_Men1, Max_Men1, Min_Men1)
+
+
+# Number of people that have chest pain 1
+Chest_Pain_One1 = No_Heart_Disease_Symptoms[No_Heart_Disease_Symptoms['Chest Pain Type'] == 1]
+print("These are all individuals with Chest Pain 1")
+print(Chest_Pain_One1)
+total_rows1 = Chest_Pain_One1.shape[0]
+print('total number of people: ', total_rows1)
+# Number of people that have chest pain 2
+Chest_Pain_Two1 = No_Heart_Disease_Symptoms[No_Heart_Disease_Symptoms['Chest Pain Type'] == 2]
+print("These are all individuals with Chest Pain 2")
+print(Chest_Pain_Two1)
+total_rows2 = Chest_Pain_Two1.shape[0]
+print('total number of people: ', total_rows2)
+# Number of people that have chest pain 3
+Chest_Pain_Three1 = No_Heart_Disease_Symptoms[No_Heart_Disease_Symptoms['Chest Pain Type'] == 3]
+print("These are all individuals with Chest Pain 3")
+print(Chest_Pain_Three1)
+total_rows3 = Chest_Pain_Three1.shape[0]
+print('total number of people: ', total_rows3)
+# Number of people that have chest pain 4
+Chest_Pain_Four1 = No_Heart_Disease_Symptoms[No_Heart_Disease_Symptoms['Chest Pain Type'] == 4]
+print("These are all individuals with Chest Pain 4")
+total_rows4 = Chest_Pain_Four1.shape[0]
+print(Chest_Pain_Four1)
+print('total number of people: ', total_rows4)
+
+
+Blood_Pressure_HD1 = No_Heart_Disease_Symptoms.iloc[:,3]
+print(Blood_Pressure_HD1)
+#Max BP
+Max_BP1 = Blood_Pressure_HD1.max()
+#Min BP 
+Min_BP1 = Blood_Pressure_HD1.min()
+#Average BP
+AVG_BP1 = Blood_Pressure_HD1.mean()
+print('Max BP: ', Max_BP1, 'Min Bp: ', Min_BP1, 'AVG BP: ', AVG_BP1)
+
+
+#Fasting BP 0 [6]
+BP_LessThan1 = No_Heart_Disease_Symptoms[No_Heart_Disease_Symptoms['Fasting BP']==0]
+print('Blood Pressure is less than 120')
+print(BP_LessThan1)
+#FASTING BP  1 count
+
+BP1 = No_Heart_Disease_Symptoms[No_Heart_Disease_Symptoms['Fasting BP']==1]
+BP_Total1 = BP1.shape[0]
+print('Blood Pressure is greater than 120')
+print(BP_Total1)
+
+#MIN [MIN HEART RATE]
+Min_HeartRate1 = No_Heart_Disease_Symptoms.iloc[:,7]
+Minimum_HR1 =Min_HeartRate1.min()
+print('Thia is the min heart rate: ', Minimum_HR1)
+#MAX [MIN HEART RATE]
+Max_HeartRate1 = No_Heart_Disease_Symptoms.iloc[:,7]
+Maximum_HeartRate1 =Max_HeartRate1.max()
+print('Thia is the max heart rate: ', Maximum_HeartRate1)
+# NUMBER OF EXCERSICE INDUCED ANGIA 
+Angina1 = No_Heart_Disease_Symptoms[No_Heart_Disease_Symptoms['Angia'] == 1]
+print('Patients with Induced Angina')
+print(Angina1)
+
+Not_Angina1 = No_Heart_Disease_Symptoms[No_Heart_Disease_Symptoms['Angia'] == 0]
+print('Patients without Induced Angina')
+print(Not_Angina1)
